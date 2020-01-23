000a7899e595e0ecf4627df72c4e7b908724391f
Meghan <meghan.e.mccue@gmail.com>
2020-01-23 13:03:54 +0000

    Add jquery

--- src/interface.js
+++ src/interface.js
@@ -1,8 +1,40 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
-  $("#current-temp-display").text(thermostat.temp);
+  $("#temperature").text(thermostat.temp);
+
   $("#temp-up").on("click", function() {
+    if (thermostat.isMaxTemp()) {
+      alert('Maximum Temperature Reached');
+    }
     thermostat.up();
-    $("#current-temp").text(thermostat.temp);
+    updateTemp()
   });
+
+  $("#temp-down").on("click", function() {
+    if (thermostat.isMinTemp()) {
+      alert('Minimum Temperature Reached');
+    }
+    thermostat.down();
+    updateTemp()
+  });
+
+  $("#temp-reset").on("click", function() {
+    thermostat.reset();
+    updateTemp()
+  });
+
+  $('input[type="checkbox"').click(function() {
+    if ($(this).is(":checked")) {
+        thermostat.powerSavingMode("On");
+        alert("CHeckbox is checked.");
+    } else if ($(this).is(":not(:checked")) {
+        thermostat.powerSavingMode("Off");
+        alert("Checkbox us unchecked.");
+    }
+});
+
+  function updateTemp() {
+    $("#temperature").text(thermostat.temp);
+    $("#temperature").attr('class', thermostat.currentEnergyUsage())
+  }
 });
